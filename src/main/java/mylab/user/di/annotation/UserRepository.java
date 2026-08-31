e592f6b0dce8d1c24506d7e4210fbbdf1c429c32
bluejals13 <yoriya10113@gmail.com>
2026-08-31 19:22:42 +0900

    Update UserRepository.java

--- src/main/java/mylab/user/di/annotation/UserRepository.java
+++ src/main/java/mylab/user/di/annotation/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserRepository {
 
-    @Value("${DB_TYPE:MySQL}")
+    @Value("MySQL")
     private String dbType;
 
     @Autowired(required = false)
